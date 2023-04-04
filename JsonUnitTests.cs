e3f0b033c45e9ada67df56754b81d93b851dae23
unknown <guillianvibert90@gmail.com>
2023-04-04 21:22:30 +0200

    refactor; change namespaces

---
+++ JsonUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using Json;
+using JsonPart.Records;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace MailCreatorTests
+{
+    public class JsonUnitTests
+    {
+        [Fact]
+        public void CreateFolder()
+        {
+            Assert.True(JsonFileUtils.CreateDataFolder());
+        }
+        [Fact]
+        public void Write()
+        {
+            Assert.Throws<NullReferenceException>(() => JsonFileUtils.Write(default(object), typeof(object), "data.json"));
+            Assert.Throws<NullReferenceException>(() => JsonFileUtils.Write(default(string), typeof(string), "dat5623a.json"));
+            Assert.Throws<ArgumentException>(() => JsonFileUtils.Write(new Professeur("d", "n", "d", "n"), typeof(Professeur), "proo.f.json"));
+            Assert.Throws<TypeAccessException>(() => JsonFileUtils.Write(new Professeur("d", "n", "d", "n"), typeof(int), "dat5623a.json"));
+        }
+        [Fact]
+        public void Read()
+        {
+            Assert.Throws<FileNotFoundException>(() => JsonFileUtils.Read<Object>("data.json"));
+            Assert.Throws<ArgumentException>(() => JsonFileUtils.Read<Professeur>("datsd45a.jsn"));
+            Assert.Throws<NullReferenceException>(() => JsonFileUtils.Read<Professeur>("deleteme.json"));
+            Assert.IsType(typeof(List<Professeur>),JsonFileUtils.Read<List<Professeur>>("professeurs.json"));
+        }
+    }
+}
---
+++ StringUnitTests.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils;
+using Xunit;
+
+namespace MailCreatorTests
+{
+    public class StringUnitTests
+    {
+        [Fact]
+        public void MatchMailtypeText()
+        {
+            Assert.Equal("", ("").MatchMailtypeText());
+            Assert.Equal(("1").MatchMailtypeText(),"1");
+            Assert.Equal(("123456789").MatchMailtypeText(),"123456789" );
+            Assert.Equal(("aBcDeF1D35").MatchMailtypeText(),"aBcDeF1D35");
+            Assert.Equal(("gvv.sqd@mmm.comm").MatchMailtypeText(),"gvv.sqd@mmm.comm");
+            Assert.Equal(("gvv.sq_d@mmm.comm").MatchMailtypeText(),"gvv.sq_d@mmm.comm");
+            Assert.Equal(("g-vv.sq_d@mmm.comm").MatchMailtypeText(),"g-vv.sq_d@mmm.comm" );
+            Assert.NotEqual("g-v$v.sq_d@mmm.comm", ("g-v$v.sq_d@mmm.comm").MatchMailtypeText());
+            Assert.NotEqual(("ffdsq*.").MatchMailtypeText(),"ffdsq*.");
+            Assert.Equal("", ("458<è'é\"rfdsdsf").MatchMailtypeText());
+        }
+        [Fact]
+        public void MatchJsonFilename()
+        {
+            Assert.Throws<ArgumentException>(() => "sq.dsqf.json".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => "/dsqf.json".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => "./45.json".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => ".json".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => "sqdd52".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => "sqdd52.jsonn".MatchJsonFilename());
+            Assert.Throws<ArgumentException>(() => " .json".MatchJsonFilename());
+            Assert.Equal("data.json", ("data.json").MatchJsonFilename());
+            Assert.Equal("dat452dAcfdsa.json", ("dat452dAcfdsa.json").MatchJsonFilename());
+        }
+    }
+}
